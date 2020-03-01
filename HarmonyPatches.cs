4a8c3c1cffb971a187869ee1ce5c938a9e34a7c2
Dingo <oferdottan@gmail.com>
2020-03-01 03:52:52 +0200

    RW 1.1

---
+++ HarmonyPatches.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Verse;
+
+namespace Persistent_Float_Menus
+{
+	[StaticConstructorOnStartup]
+	public static class HarmonyPatches
+	{
+		private static readonly FieldInfo GetModifiedDistanceToClose = AccessTools.Field(typeof(Settings), nameof(Settings.ModifiedDistanceToClose));
+
+		static HarmonyPatches()
+		{
+#if DEBUG
+			Harmony.DEBUG = true;
+#endif
+
+			Harmony harmony = new Harmony("dingo.persistentfloatmenus");
+
+			harmony.PatchAll(Assembly.GetExecutingAssembly());
+		}
+
+		[HarmonyPatch(typeof(FloatMenu))]
+		[HarmonyPatch("UpdateBaseColor")]
+		public static class Patch_Transpiler_FloatMenu
+		{
+			public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+			{
+				foreach (CodeInstruction code in instructions)
+				{
+					// ldc.r4 - Pushes a supplied value of type float32 onto the evaluation stack as type float
+					if (code.opcode == OpCodes.Ldc_R4 && (float)code.operand == Settings.DefaultClosingMouseDist)
+					{
+						// ldsfld - Pushes the value of a static field onto the evaluation stack
+						CodeInstruction replacement = new CodeInstruction(OpCodes.Ldsfld, GetModifiedDistanceToClose);
+
+						yield return replacement;
+					}
+
+					else
+					{
+						yield return code;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Persistent Float Menus")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Persistent Float Menus")]
+[assembly: AssemblyCopyright("Copyright © Dingo 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2ba37272-6b69-49ec-a009-db856979b4cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.1.7364.1")]
+[assembly: AssemblyFileVersion("1.1.7364.1")]
